de1b7ec3abaf8e9b95dbdbeb384c15331d8ae957
absolutesub <absolutefansubbackup@gmail.com>
2025-08-03 15:13:32 -0300

    ...

--- js/animePag.js
+++ js/animePag.js
@@ -156,7 +156,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const iconeDownload = document.createElement('span');
             iconeDownload.className = 'icone-download';
-            iconeDownload.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M480-328.46 309.23-499.23l42.16-43.38L450-444v-336h60v336l98.61-98.61 42.16 43.38L480-328.46ZM252.31-180Q222-180 201-201q-21-21-21-51.31v-108.46h60v108.46q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85h455.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46v-108.46h60v108.46Q780-222 759-201q-21 21-51.31 21H252.31Z"/></svg>';
+            iconeDownload.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M480-328.46 309.23-499.23l42.16-43.38L450-444v-336h60v336l98.61-98.61 42.16 43.38L480-328.46ZM252.31-180Q222-180 201-201q-21-21-21-51.31v-108.46h60v108.46q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85h455.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46v-108.46h60v108.46Q780-222 759-201q-21 21-51.31 21H252.31Z"/></svg>`;
 
             cabecalho.appendChild(titulo);
             cabecalho.appendChild(iconeDownload);
@@ -179,9 +179,25 @@ document.addEventListener('DOMContentLoaded', () => {
                     a.target = '_blank';
                     a.rel = 'noopener noreferrer';
                     a.innerHTML = `
-                        ${plataforma.icone}
-                        <span>${plataforma.nome}</span>
-                    `;
+                ${plataforma.icone}
+                <span>${plataforma.nome}</span>
+            `;
+
+                    // Adiciona os atributos para rastreamento
+                    a.dataset.anime = anime.titulo;
+                    a.dataset.episodio = ep.numero;
+                    a.dataset.plataforma = plataforma.nome;
+
+                    // Evento de clique para GA4
+                    a.addEventListener('click', () => {
+                        gtag('event', 'clique_download', {
+                            event_category: 'download',
+                            event_label: anime.titulo,
+                            episodio: ep.numero,
+                            plataforma: plataforma.nome
+                        });
+                    });
+
                     botoes.appendChild(a);
                 }
             });
