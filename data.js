470af7a532b24509e7a494a875966e85784ac0f8
absolutesub <absolutefansubbackup@gmail.com>
2025-08-07 12:06:19 -0300

    Novo post via painel

--- data.js
+++ data.js
@@ -1,72 +1,29 @@
 [
   {
-    "id": 2,
-    "titulo": "test",
-    "imagem": null,
+    "id": 1,
+    "titulo": "tst",
+    "imagem": "img/id1/id1capa.webp",
     "tags": [
-      "test"
+      "tst"
     ],
-    "lancamento": "2025",
-    "genero": "test",
-    "episodiosTotal": 2,
+    "lancamento": "2023",
+    "genero": "tst",
+    "episodiosTotal": 3,
     "status": "Finalizado",
-    "sinopse": "test",
-    "trailerUrl": "test",
-    "adicionadoEm": "2025-08-07T12:02:28.915567",
-    "estudio": "test",
-    "siteOficial": "test",
+    "sinopse": "tsttst",
+    "trailerUrl": "tst",
+    "adicionadoEm": "2025-08-07T12:06:19.472198",
+    "estudio": "tst",
+    "siteOficial": "tst",
     "video": "x264 10Bit",
     "audio": "AAC",
     "resolucao": "1080p",
-    "legendas": "Softsub",
-    "scriptOriginal": "test",
-    "form_url": "test",
-    "entry_nome": "test",
-    "entry_comentario": "test",
-    "csv_url": "test",
-    "screenshots": [],
-    "episodios": [
-      {
-        "numero": 1,
-        "downloads": {
-          "krakenfiles": "test",
-          "nyaa": "test"
-        }
-      },
-      {
-        "numero": 2,
-        "downloads": {
-          "krakenfiles": "testtest",
-          "nyaa": "test"
-        }
-      }
-    ]
-  },
-  {
-    "id": 1,
-    "titulo": "test",
-    "imagem": "img/id1/id1capa.webp",
-    "tags": [
-      "test"
-    ],
-    "lancamento": "2020",
-    "genero": "test",
-    "episodiosTotal": 2,
-    "status": "Em andamento",
-    "sinopse": "test",
-    "trailerUrl": "test",
-    "adicionadoEm": "2025-08-07T12:01:53.538163",
-    "estudio": "tees",
-    "siteOficial": "test",
-    "video": "x264 10Bit",
-    "audio": "AAC",
-    "resolucao": "480p",
     "legendas": "HardSub",
-    "scriptOriginal": "test",
-    "form_url": "test",
-    "entry_nome": "test",
-    "entry_comentario": "test",
-    "csv_url": "test",
+    "scriptOriginal": "tst",
+    "form_url": "tst",
+    "entry_nome": "tst",
+    "entry_comentario": "tst",
+    "csv_url": "tst",
     "screenshots": [
       "img/id1/shot1.webp",
       "img/id1/shot2.webp",
@@ -76,15 +33,22 @@
       {
         "numero": 1,
         "downloads": {
-          "pcloud": "test",
-          "terabox": "test"
+          "pixeldrain": "tst",
+          "terabox": "tst"
         }
       },
       {
         "numero": 2,
         "downloads": {
-          "pcloud": "test",
-          "terabox": "test"
+          "pixeldrain": "tst",
+          "terabox": "tst"
+        }
+      },
+      {
+        "numero": 3,
+        "downloads": {
+          "pixeldrain": "tst",
+          "terabox": "tst"
         }
       }
     ]
