ade5d48e1573fbd93f95dc8384e4f0dad4f5a8bb
absolutesub <absolutefansubbackup@gmail.com>
2025-09-01 21:49:43 -0300

    Adicionou/Editou o anime ID 32

--- data.js
+++ data.js
@@ -1,5 +1,41 @@
 const animes = [
   {
+    "id": 32,
+    "adicionadoEm": "01 de setembro de 2025",
+    "imagem": "img/id32/id32capa.webp",
+    "banner": null,
+    "screenshots": [],
+    "episodios": [
+      {
+        "numero": 1,
+        "downloads": {}
+      },
+      {
+        "numero": 2,
+        "downloads": {}
+      }
+    ],
+    "titulo": "test",
+    "genero": "",
+    "tags": [],
+    "lancamento": "2025",
+    "sinopse": "",
+    "status": "Finalizado",
+    "estudio": "",
+    "resolucao": "1080p",
+    "video": "x264 10Bit",
+    "audio": "AAC",
+    "legendas": "Softsub",
+    "scriptOriginal": "",
+    "siteOficial": "",
+    "trailerUrl": "",
+    "form_url": "",
+    "entry_nome": "",
+    "entry_comentario": "",
+    "csv_url": "",
+    "episodiosTotal": 2
+  },
+  {
     "id": 31,
     "adicionadoEm": "09 de agosto de 2025",
     "imagem": "img/id31/id31capa.webp",
