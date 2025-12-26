684f1c1916a21c42984d8bb8264fa6cf36bc1270
Absolute <absolutefansubbackup@gmail.com>
2025-12-26 18:14:36 -0300

    Delete admin directory

--- admin/admin.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById('form').onsubmit=e=>{
- e.preventDefault();
- fetch('/api/content',{method:'POST',headers:{'Content-Type':'application/json'},
- body:JSON.stringify({title:title.value,content:content.value})})
- alert('Conteúdo enviado');
-}
