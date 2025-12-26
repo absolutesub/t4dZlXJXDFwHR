4d58500fff196406b91529f1a2211ca41aaec814
Absolute <absolutefansubbackup@gmail.com>
2025-12-26 18:22:10 -0300

    Delete frontend/js directory

--- frontend/js/favorites.js
+++
@@ -1,6 +0,0 @@
-
-export function toggleFavorite(id){
- const f=JSON.parse(localStorage.getItem('fav'))||[];
- const n=f.includes(id)?f.filter(x=>x!==id):[...f,id];
- localStorage.setItem('fav',JSON.stringify(n));
-}
