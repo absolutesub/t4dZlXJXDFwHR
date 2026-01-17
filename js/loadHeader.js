eb05ad3ef1c7432559ff61ac99954782a5629651
Absolute <absolutefansubbackup@gmail.com>
2026-01-17 18:22:18 -0300

    Add dynamic header creation on DOMContentLoaded

--- js/loadHeader.js
+++ js/loadHeader.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.createElement("header");
+  header.className = "site-header";
+
+  header.innerHTML = `
+    <div class="container">
+      <h1 class="logo">
+        <a href="index.html">Absolute Fansub</a>
+      </h1>
+
+      <nav class="main-nav">
+        <ul>
+          <li><a href="index.html">Início</a></li>
+          <li><a href="post2.html">Posts</a></li>
+        </ul>
+      </nav>
+    </div>
+  `;
+
+  document.body.insertBefore(header, document.body.firstChild);
+});
 
